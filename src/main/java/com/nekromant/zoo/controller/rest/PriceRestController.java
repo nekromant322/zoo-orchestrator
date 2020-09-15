558867a907b4987a52fa97e5a31a77f9e620bc9c
Виталий Черников <chernickovvit@yandex.ru>
2020-09-15 22:38:54 +0300

    Calculate Price + Tolya Popravki

--- src/main/java/com/nekromant/zoo/controller/rest/PriceRestController.java
+++ src/main/java/com/nekromant/zoo/controller/rest/PriceRestController.java
@@ -1,11 +1,15 @@
 package com.nekromant.zoo.controller.rest;
 
+import com.nekromant.zoo.model.AnimalRequest;
 import com.nekromant.zoo.model.Price;
 import com.nekromant.zoo.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import static com.nekromant.zoo.enums.RoomType.*;
 
 @RestController
 @RequestMapping("/api/price")
@@ -19,4 +23,28 @@ public class PriceRestController {
         return priceService.getActualPrice();
     }
 
+    @PostMapping("/calc")
+    public int calculateTotalPriceForRequest(@RequestBody AnimalRequest animalRequest) {
+
+        LocalDate begin = animalRequest.getBeginDate();
+        LocalDate end = animalRequest.getEndDate();
+        Period period = Period.between(begin, end);
+        int difference = period.getDays();
+        int price = 0;
+
+        if (animalRequest.getRoomType() == COMMON) {
+            price = difference * priceService.getActualPrice().getCommonRoomPrice();
+        }
+        if (animalRequest.getRoomType() == LARGE) {
+            price = difference * priceService.getActualPrice().getLargeRoomPrice();
+        }
+        if (animalRequest.getRoomType() == VIP) {
+            price = difference * priceService.getActualPrice().getVipRoomPrice();
+        }
+        if (animalRequest.getVideoNeeded() == true) {
+            price += priceService.getActualPrice().getVideoPrice();
+        }
+
+        return price;
+    }
 }
