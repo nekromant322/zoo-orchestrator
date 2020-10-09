af70d907934ba46fd3d91d10920c2c65cf8aa0e3
Alexander <san4ows55@gmail.com>
2020-10-09 22:20:59 +0300

    getter

--- src/main/java/com/nekromant/zoo/service/ContactsService.java
+++ src/main/java/com/nekromant/zoo/service/ContactsService.java
@@ -1,7 +1,13 @@
 package com.nekromant.zoo.service;
 
+import com.nekromant.zoo.controller.ContactsController;
+import org.asynchttpclient.ClientStats;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ContactsService {
@@ -16,4 +22,27 @@ public class ContactsService {
     private float LAT;
     @Value("${contacts.lng}")
     private float LNG;
+    HashMap<String, String> contactsInfo = new HashMap<String, String>();
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+    public String getPHONE_NUMBER() {
+        return PHONE_NUMBER;
+    }
+    public String getLOCATION() {
+        return LOCATION;
+    }
+    public float getLAT() {
+        return LAT;
+    }
+    public float getLNG() {
+        return LNG;
+    }
+
+
+//    contactsInfo.put()
+//    public HashMap<String, String> getContactsInfo() {
+//
+//    }
 }
