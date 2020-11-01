226c1001783633fb55574c51a0462c095c0f1042
l1dex <l1dex@github.com>
2020-11-02 01:51:14 +0300

    Always passed test

--- src/test/java/com/example/testdb/TestdbApplicationTests.java
+++ src/test/java/com/example/testdb/TestdbApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.testdb;
 
+import com.nekromant.zoo.ZooApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ZooApplication.class)
 public class TestdbApplicationTests {
 
     @Test
