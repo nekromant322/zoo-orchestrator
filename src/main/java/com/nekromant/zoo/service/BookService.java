0c074374ec2474bf356d052d419764a3a05eb65f
l1dex <l1dex@github.com>
2020-11-16 15:46:43 +0300

    transaction added

--- src/main/java/com/nekromant/zoo/service/BookService.java
+++ src/main/java/com/nekromant/zoo/service/BookService.java
@@ -7,6 +7,7 @@ import com.nekromant.zoo.model.Book;
 import com.nekromant.zoo.model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -30,6 +31,7 @@ public class BookService {
      * @param room - {@link Room} room to book
      * @return new {@link Book} or null
      */
+    @Transactional
     public Book bookAnimalRequest(String id, Room room) {
         AnimalRequest animalRequest = animalRequestService.findById(id);
         return bookRoom(room, animalRequest);
@@ -46,6 +48,7 @@ public class BookService {
         bookDAO.save(book);
 
         animalRequestService.setInProgressAnimalRequest(animalRequest.getId().toString());
+
         return book;
     }
 
