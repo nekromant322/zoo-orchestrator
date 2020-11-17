b54cae33c77d3e6670e069c79611351986dc7b99
l1dex <l1dex@github.com>
2020-11-17 19:13:14 +0300

    swagger doc for endpoints RoomRestController, BookRestController

--- src/main/java/com/nekromant/zoo/controller/rest/BookRestController.java
+++ src/main/java/com/nekromant/zoo/controller/rest/BookRestController.java
@@ -3,6 +3,8 @@ package com.nekromant.zoo.controller.rest;
 import com.nekromant.zoo.model.Book;
 import com.nekromant.zoo.model.Room;
 import com.nekromant.zoo.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,15 +12,29 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@Api(value = "book")
 public class BookRestController {
     @Autowired
     private BookService bookService;
 
+    @ApiOperation(
+            value = "find all books in db",
+            produces = "json",
+            httpMethod = "GET",
+            response = Book.class,
+            responseContainer = "List"
+    )
     @GetMapping
     public List<Book> getBooks(){
         return bookService.findAll();
     }
 
+    @ApiOperation(
+            value = "booking room with AnimalRequest id",
+            produces = "json",
+            httpMethod = "POST",
+            response = Book.class
+    )
     @PostMapping("/bookRoom/{id}")
     public Book book(@PathVariable String id, @RequestBody Room room){
         return bookService.bookAnimalRequest(id,room);
--- src/main/java/com/nekromant/zoo/controller/rest/RoomRestController.java
+++ src/main/java/com/nekromant/zoo/controller/rest/RoomRestController.java
@@ -5,6 +5,8 @@ import com.nekromant.zoo.enums.AnimalType;
 import com.nekromant.zoo.enums.RoomType;
 import com.nekromant.zoo.model.Room;
 import com.nekromant.zoo.service.RoomService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,20 +15,42 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/room")
+@Api(value = "room")
 public class RoomRestController {
     @Autowired
     private RoomService roomService;
 
+    @ApiOperation(
+            value = "get all rooms from db",
+            response = Room.class,
+            responseContainer = "List",
+            httpMethod = "GET",
+            produces = "json"
+    )
     @GetMapping
     List<Room> getAll(){
         return roomService.findAll();
     }
 
+    @ApiOperation(
+            value = "add room to db from request body",
+            response = Room.class,
+            httpMethod = "POST",
+            produces = "json"
+    )
     @PostMapping
     Room addRoom(@RequestBody Room room){
         return roomService.insert(room);
     }
 
+    @ApiOperation(
+            value = "get rooms by parameters",
+            notes = "find by Parameters in RoomParametersDTO such as AnimalType, RoomType, videoNeeded begin-end date",
+            response = Room.class,
+            responseContainer = "List",
+            httpMethod = "POST",
+            produces = "json"
+    )
     @PostMapping("/spareRooms")
     public List<Room> getSpareRoom(@RequestBody RoomParametersDTO roomParametersDTO){
         return roomService.findAllSpareRoom(roomParametersDTO);
