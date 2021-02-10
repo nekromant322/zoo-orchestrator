bf1d49df37bc1babb09cdda2eb566a277fcb2e22
andrienko-anfisa <andrienko-anfisa@users.noreply.github.com>
2021-02-10 10:54:28 +0300

    Добавлена возможность отправлять email

---
+++ src/main/java/com/nekromant/zoo/controller/rest/EmailRestController.java
@@ -0,0 +1,29 @@
+package com.nekromant.zoo.controller.rest;
+
+
+import com.nekromant.zoo.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/api/emailPage")
+@RestController
+public class EmailRestController {
+
+    @Autowired
+    EmailService emailService;
+
+    @GetMapping("/newEmail")
+    public String sendEmail() {
+        String emailReceiver = "anfisa.andrienko.aa@gmail.com";
+        String emailSubject = "Test Simple Email";
+        String emailText = "Hello, Im testing Simple Email";
+        emailService.sendEmail(emailReceiver, emailSubject, emailText);
+        return "Email Sent!";
+
+    }
+}
