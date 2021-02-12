260e8ebed2a68b38bc3b1dd94f8d2b0dfc7a2f0e
l1dex <l1dex@github.com>
2021-02-12 21:38:36 +0300

    little change

--- src/main/java/com/nekromant/zoo/controller/rest/AnimalRequestRestController.java
+++ src/main/java/com/nekromant/zoo/controller/rest/AnimalRequestRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/animalRequestPage")
 public class AnimalRequestRestController {
 
+
     @Autowired
     private AnimalRequestService animalRequestService;
 
