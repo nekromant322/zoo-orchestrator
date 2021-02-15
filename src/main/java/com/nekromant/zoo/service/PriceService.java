a49df69f629a8daa463c84b9cd1e1efc1f5e6859
andrienko-anfisa <andrienko-anfisa@users.noreply.github.com>
2021-02-16 00:52:42 +0300

    Рефакторинг сервиса цен

--- src/main/java/com/nekromant/zoo/service/PriceService.java
+++ src/main/java/com/nekromant/zoo/service/PriceService.java
@@ -2,6 +2,7 @@ package com.nekromant.zoo.service;
 
 import com.nekromant.zoo.dao.PriceDAO;
 import com.nekromant.zoo.enums.AnimalType;
+import com.nekromant.zoo.enums.RoomType;
 import com.nekromant.zoo.model.AnimalRequest;
 import com.nekromant.zoo.model.Price;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,29 +43,40 @@ public class PriceService {
      * @param animalRequest заявка
      * @return Полная стоимость заявки
      */
-    public int calculateTotalPrice(AnimalRequest animalRequest) {
+    public int getAnimalTypePrice(AnimalRequest animalRequest) {
+        LocalDate begin = animalRequest.getBeginDate();
+        LocalDate end = animalRequest.getEndDate();
+        int difference = daysBetween(begin, end);
+        int price = 0;
+        Map<AnimalType, Integer> animalTypePrice = new HashMap<>();
+        animalTypePrice.put(CAT, priceService.getActualPrice().getCatPrice());
+        animalTypePrice.put(DOG, priceService.getActualPrice().getDogPrice());
+        animalTypePrice.put(REPTILE, priceService.getActualPrice().getReptilePrice());
+        animalTypePrice.put(RAT, priceService.getActualPrice().getRatPrice());
+        animalTypePrice.put(BIRD, priceService.getActualPrice().getBirdPrice());
+        animalTypePrice.put(OTHER, priceService.getActualPrice().getOtherPrice());
 
+        price += difference * animalTypePrice.get(animalRequest.getAnimalType());
+        return price;
+    }
+
+
+    public int getRoomTypePrice(AnimalRequest animalRequest) {
         LocalDate begin = animalRequest.getBeginDate();
         LocalDate end = animalRequest.getEndDate();
         int difference = daysBetween(begin, end);
         int price = 0;
 
-        Map<AnimalType,Integer> animalTypePrice = new HashMap<>();
-        animalTypePrice.put(CAT,priceService.getActualPrice().getCatPrice());
-        animalTypePrice.put(DOG,priceService.getActualPrice().getDogPrice());
-        animalTypePrice.put(REPTILE,priceService.getActualPrice().getReptilePrice());
-        animalTypePrice.put(RAT,priceService.getActualPrice().getRatPrice());
-        animalTypePrice.put(BIRD,priceService.getActualPrice().getBirdPrice());
-        animalTypePrice.put(OTHER,priceService.getActualPrice().getOtherPrice());
+        Map<RoomType, Integer> roomTypePrice = new HashMap<>();
+        roomTypePrice.put(LARGE, priceService.getActualPrice().getLargeRoomPrice());
+        roomTypePrice.put(VIP, priceService.getActualPrice().getVipRoomPrice());
+        roomTypePrice.put(COMMON, priceService.getActualPrice().getCommonRoomPrice());
+        price += difference * roomTypePrice.get(animalRequest.getRoomType());
+        return price;
+    }
 
-        price+= difference * animalTypePrice.get(animalRequest.getAnimalType());
-
-        if (animalRequest.getRoomType() == LARGE) {
-            price+= difference * priceService.getActualPrice().getLargeRoomPrice();
-        }
-        if (animalRequest.getRoomType() == VIP) {
-            price += difference * priceService.getActualPrice().getVipRoomPrice();
-        }
+    public int calculateTotalPrice(AnimalRequest animalRequest) {
+        int price = getAnimalTypePrice(animalRequest)+getRoomTypePrice(animalRequest);
         if (animalRequest.getVideoNeeded()) {
             price += priceService.getActualPrice().getVideoPrice();
         }
