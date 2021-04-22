3da217a4453fc6431d2b86d4cdebe7d34a75d020
CeJoW <32615273+CeJoW@users.noreply.github.com>
2021-04-22 15:00:53 +0300

    Добавлен логгер в приложение

--- Zoo-orchestrator/src/main/java/com/nekromant/zoo/service/BlackListService.java
+++ Zoo-orchestrator/src/main/java/com/nekromant/zoo/service/BlackListService.java
@@ -3,12 +3,14 @@ package com.nekromant.zoo.service;
 import com.nekromant.zoo.dao.BlackListDAO;
 import com.nekromant.zoo.model.AnimalRequest;
 import com.nekromant.zoo.model.BlackList;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class BlackListService {
     @Autowired
@@ -21,13 +23,15 @@ public class BlackListService {
     /**
      * Find animalRequest by id
      * Creates Black list with email and phone number animalRequest found
+     *
      * @param id primary index of animalRequest
      */
     public void insertByAnimalRequestId(String id) {
         Optional<AnimalRequest> animalRequest = animalRequestService.findById(id);
-        if(animalRequest.isPresent()) {
+        if (animalRequest.isPresent()) {
             blackListDAO.save(new BlackList(0, animalRequest.get().getEmail(), animalRequest.get().getPhoneNumber()));
             animalRequestService.declineAnimalRequest(id);
+            log.info("Номер телефона: {} и email: {} добавлены в чёрный список", animalRequest.get().getPhoneNumber(), animalRequest.get().getEmail());
         }
     }
 
